f9d5aada24e7e2425b1ec881de0ad8f82b40810f
Carmen1707 <cmunvel574@g.educaand.es>
2025-01-17 14:35:35 +0100

    4

---
+++ e6.py
@@ -0,0 +1,9 @@
+n1 = int(input("Introduce el primer numero"))
+n2 = int(input("Introduce el segundo numero"))
+if n1>n2:
+    print ( n1, "es mayor que" , n2)
+elif n1<n2:
+    print( n1 ,"es menor que" ,n2)
+else:
+    print( n1 ,"es igual" ,n2)
+
