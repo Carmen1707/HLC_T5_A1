30e6995950aa155493a42de880f1c9713eb7ab0b
Carmen1707 <cmunvel574@g.educaand.es>
2025-01-21 13:10:36 +0100

    8

--- Boletín 2/1.py
+++ Boletín 2/1.py
@@ -1,2 +1,5 @@
-usuario = input("Introduce tu usuario:")
-print("Hola,", usuario)
+edadv = int(input("Introduce tu edad:"))
+if edadv <= 17:
+    print("No puedes votar.")
+else:
+    print("Puedes votar.")
--- Boletín 2/2.py
+++ Boletín 2/2.py
@@ -1,4 +1,7 @@
-suma1 = int(input("Introduce el primer numero:"))
-suma2 = int(input("Introduce el segundo numero:"))
-sumat = suma1 + suma2
-print ("La suma de", suma1,"y" , suma2 ,"es:" ,sumat)
+n1 = int(input("Introduce el primer numero:"))
+n2 = int(input("Introduce el segundo numero:"))
+n3 = int(input("Introduce el tercer numero:"))
+if n1<n2 & n1<n3:
+    print ( n1, "es mayor que" , n2 ,"y" ,n3)
+else :
+    print( n1 ,"es menor que" ,n2 , "y" ,n3)
